f4b0ea17419de3c50e3f58494407f9701f415567
Ramzi-dr <82414280+Ramzi-dr@users.noreply.github.com>
2021-07-11 18:38:01 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,31 @@
+
+#https://opentdb.com/
+question_data = {"response_code": 0, "results": [
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The common software-programming acronym &quot;I18N&quot; comes from the term "
+                 "&quot;Interlocalization&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The first computer bug was formed by faulty wires.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "The very first recorded computer &quot;bug&quot; was a moth found "
+                                                  "inside a Harvard Mark II computer.",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "&quot;Windows NT&quot; is a monolithic kernel.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "To bypass US Munitions Export Laws, the creator of the PGP published"
+                                                  " all the source code in book form. ",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "MacOS is based on Linux.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "AMD created the first consumer 64-bit processor.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                       "question": "The first dual-core CPU was the Intel Pentium D.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                       "question": "A Boolean value of &quot;0&quot; represents which of these words?",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]}]}
